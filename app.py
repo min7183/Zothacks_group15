0bcf436350c9242d03a81327364fa0e04fc56588
Jitendra <rama.jeenagala@gmail.com>
2023-11-04 19:37:14 -0700

    modified:   api_interact.py modified:   app.py modified:   templates/index.html

--- app.py
+++ app.py
@@ -19,8 +19,13 @@ def home_page(request: Request):
 
 
 @app.get("/items", response_class=HTMLResponse)
-def read_item(request: Request, search_string: str):
+def read_item(request: Request, search_string: str, keyword_string: str):
     #pictures = zip(api_interact.get_items(api_interact.get_all(search_string, media = 'image')[0]).json())
+    if keyword_string != '':
+        pictures1 = api_interact.get_items(api_interact.get_all(search_string, keywords=keyword_string).json())[0]
+        pictures2 = api_interact.get_items(api_interact.get_all(search_string, keywords=keyword_string).json())[1]
+        zip(pictures1, pictures2) 
+        return templates.TemplateResponse("item.html", {"request": request, "search_string": zip(pictures1, pictures2)})
     pictures1 = api_interact.get_items(api_interact.get_all(search_string).json())[0]
     pictures2 = api_interact.get_items(api_interact.get_all(search_string).json())[1]
     zip(pictures1, pictures2) 
